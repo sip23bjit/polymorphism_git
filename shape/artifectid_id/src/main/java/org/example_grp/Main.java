16469edfde88e844c9984770b678665ccd31ec66
Shahirul Islam Polak <131145933+sip23bjit@users.noreply.github.com>
2023-06-08 12:16:12 +0600

    Update Main.java

--- shape/artifectid_id/src/main/java/org/example_grp/Main.java
+++ shape/artifectid_id/src/main/java/org/example_grp/Main.java
@@ -1,13 +1,13 @@
 package org.example_grp;
 
-// Shape class (base class)
+
 class Shape {
     public void draw() {
         System.out.println("Drawing a shape");
     }
 }
 
-// Circle class (derived class)
+
 class Circle extends Shape {
     @Override
     public void draw() {
@@ -15,7 +15,7 @@ class Circle extends Shape {
     }
 }
 
-// Rectangle class (derived class)
+
 class Rectangle extends Shape {
     @Override
     public void draw() {
@@ -23,25 +23,13 @@ class Rectangle extends Shape {
     }
 }
 
-// Triangle class (derived class)
-class Triangle extends Shape {
-    @Override
-    public void draw() {
-        System.out.println("Drawing a triangle");
-    }
-}
 
-// Main class
 public class Main {
     public static void main(String[] args) {
-        // Creating objects of different shapes
         Shape shape1 = new Circle();
         Shape shape2 = new Rectangle();
-        Shape shape3 = new Triangle();
+        shape1.draw(); 
+        shape2.draw(); 
 
-        // Calling the draw() method on each shape
-        shape1.draw(); // calls the draw() method in the Circle class
-        shape2.draw(); // calls the draw() method in the Rectangle class
-        shape3.draw(); // calls the draw() method in the Triangle class
     }
 }
